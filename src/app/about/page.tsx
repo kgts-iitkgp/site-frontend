1773d9d587f13cfb1aaa81b6ffa47052630c6ea4
Shankit324 <kdshankit11058@gmail.com>
2024-12-15 10:38:39 +0530

    updating responsive fonts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,22 +7,23 @@ import { GoCommentDiscussion } from "react-icons/go";
 import { FaBookOpen } from "react-icons/fa";
 import { FaGamepad } from "react-icons/fa";
 
+
 export default function About() {
   return (
-    <div>
+    <div className="flex justify-center">
       <DotBackground/>
-      <div className="relative w-full h-auto inline-block">
+      <div className="relative w-full h-full inline-block">
         <img 
           src="/images/fullteam.jpg"
-          style={{opacity: "0.4", height: "auto", width: "100%"}}
+          style={{opacity: "1", height: "auto", width: "100%", }}
         ></img>
-        <div className="absolute h-auto w-full top-10 md:top-[170px] xl:top-[330px] text-center">
-          <div className="font-extrabold font-sans text-6xl text-white">ABOUT US</div>
-          <div className="w-full align-middle flex flex-row justify-center"><div className="w-4/5 md:w-2/5 text-lg text-gray-400 my-6 ">
+        <div style={{backgroundColor: "rgba(0, 0, 0, 0.6)"}} className="absolute h-[10%] sm:h-[21%] md:h-[34%] w-full pt-3 md:pt-[170px] xl:pt-[330px] top-0 text-center">
+          <div className="font-extrabold font-sans text-4xl sm:text-6xl text-primary top-10">ABOUT US</div>
+          <div className="w-full align-middle flex flex-row justify-center"><div className="w-4/5 md:w-2/5 text-gray-400 text-lg my-6">
             <b>The KGPian Game Theory Society</b>, abbreviated as <b>KGTS</b>,
-            is <b>IIT Kharagpur’s game theory organisation.</b> <p className="hidden sm:block">It is a dynamic and
+            is <b>IIT Kharagpur’s game theory organisation.</b><span className="hidden sm:block">It is a dynamic and
             intellectually stimulating community for students passionate about
-            the study and application of game theory.</p>
+            the study and application of game theory.</span>
           </div></div>
         </div>
         <div className="flex flex-col justify-center items-center">
@@ -33,14 +34,14 @@ export default function About() {
           </div>
          <div>
           <div className="w-full md:w-4/5 h-auto px-8 py-4 sm:p-8 flex flex-col sm:flex-row justify-center items-center">
-            <div className="w-full sm:w-1/2 h-auto object-cover overflow-hidden"><img
+            <div className="w-4/5 sm:w-1/2 h-auto object-cover overflow-hidden"><img
               src="/images/heads.jpg"
               alt="Image 2"
               className="transition-transform duration-500 ease-in-out transform hover:scale-110 hover:filter hover:grayscale hover:contrast-125"
             /></div>
-            <p className="w-full sm:w-1/2 h-auto text-base md:text-lg text-left text-gray-400 my-6 mx-6">
-              <b className="text-white">FOUNDING</b>
-              <br />
+            <p className="w-full sm:w-1/2 h-auto text-base md:text-lg text-left text-white my-6 mx-6">
+              <b className="text-tertiary text-4xl">Founding</b>
+              <br /><br />
               <b>The KGPian Game Theory Society</b> was founded on{" "}
               <b>19th August 2010</b> by <b>Manoj Gadia</b>. It is a student
               body which organizes various Game Theory related programs. With a
@@ -54,10 +55,10 @@ export default function About() {
             </p>
           </div>
           </div>
-          <div className="w-full md:w-4/5 h-auto px-8 py-4 sm:p-8 flex flex-col sm:flex-row justify-center items-center">
-          <p className="w-full sm:w-1/2 h-auto text-base md:text-lg text-left text-gray-400 my-6 mx-6">
-            <b className="text-white">MOTIVE</b>
-              <br />
+          <div className="w-full md:w-4/5 h-auto px-8 py-4 sm:p-8 flex flex-col-reverse sm:flex-row justify-center items-center">
+          <p className="w-full sm:w-1/2 h-auto text-base md:text-lg text-left text-white my-6 mx-6">
+            <b className="text-tertiary text-4xl">Motive</b>
+              <br /><br />
               The Society was founded with a main motive to promote concepts of{" "}
               <b>Game Theory</b> and <b>Strategic Thinking</b>. Many situations
               in people's lives make them choose between different options and
@@ -67,7 +68,7 @@ export default function About() {
               choose the optimal solution through various methods and
               statistics. This society promotes these concepts by various means.
             </p>
-            <div className="w-full sm:w-1/2 h-auto object-cover overflow-hidden"><img
+            <div className="w-4/5 sm:w-1/2 h-auto object-cover overflow-hidden"><img
               src="/images/fullteam.jpg"
               alt="Image 1"
               className="w-full sm:w-1/2 h-auto object-cover transition-transform duration-500 ease-in-out transform hover:scale-110 hover:filter hover:grayscale hover:contrast-125"
@@ -81,9 +82,9 @@ export default function About() {
             </h1>
           </div>
           <div className="mx-6 flex flex-col justify-center items-center">
-          <div className="flex flex-row justify-around"><div className="w-[40%] h-auto flex flex-col items-center">
+          <div className="flex flex-col sm:flex-row sm:justify-around justify-center items-center"><div className="w-4/5 sm:w-[40%] h-auto flex flex-col items-center">
           <PiStrategyBold style={{width: 100, height: 100, color: "white"}}/>
-          <p className="text-base md:text-lg  text-gray-400 my-6 text-center">
+          <p className="text-base md:text-lg  text-tertiary my-6 text-center">
               <b className="text-white">THE ART OF STRATEGY PROJECT</b><br/>
               This project is started under the advice of Prof. Avinash Dixit,
               emeritus professor of Princeton University. Society members
@@ -94,25 +95,25 @@ export default function About() {
               <br />
             </p>
           </div>
-          <div className="w-[40%] h-auto flex flex-col items-center">
+          <div className="w-4/5 sm:w-[40%] h-auto flex flex-col items-center">
           <GoCommentDiscussion style={{width: 100, height: 100, color: "white"}}/>
-          <p className="text-base md:text-lg  text-gray-400 my-6 text-center">
+          <p className="text-base md:text-lg  text-tertiary my-6 text-center">
               <b className="text-white">DISCUSSION SESSIONS</b><br/>
               In discussion sessions, senior members conduct discussion sessions
               for all the members, explaining the various theoretical concepts,
               along with interesting games and real-life examples.<br/></p>
           </div></div>
-          <div className="flex flex-row justify-around"><div className="w-[40%] h-auto flex flex-col items-center">
+          <div className="flex flex-col sm:flex-row sm:justify-around justify-center items-center"><div className="w-4/5 sm:w-[40%] h-auto flex flex-col items-center">
           <FaBookOpen style={{width: 100, height: 100, color: "white"}}/>
-          <p className="text-base md:text-lg  text-gray-400 my-6 text-center">
+          <p className="text-base md:text-lg  text-tertiary my-6 text-center">
               <b className="text-white">CASE STUDIES</b><br/>
               Society members practice solving case studies using game theory in
               our academic meetings. Senior members solve real life case studies
               in the fields of business and economics.<br/></p>
           </div>
-          <div className="w-[40%] h-auto flex flex-col items-center">
+          <div className="w-4/5 sm:w-[40%] h-auto flex flex-col items-center">
           <FaGamepad style={{width: 100, height: 100, color: "white"}}/>
-          <p className="text-base md:text-lg  text-gray-400 my-6 text-center">
+          <p className="text-base md:text-lg  text-tertiary my-6 text-center">
               <b className="text-white">GAMES</b><br/>
               In society meetings, members play strategy-based games among
               themselves to learn how to use game theory concepts in a given
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,59 @@
 import type { Config } from "tailwindcss";
-
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@shadcn/ui/dist/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   darkMode: 'class',
+
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        primary: "#8c30e3",
-        secondary: "#7564ef"
+        primary: "#00BFFF",
+        secondary: "#00BFFF",
+        tertiary: '#c4e1f6',
+      },
+      boxShadow: {
+        glow: 'inset 0 0 20px #D4F6FF, inset 0 0 15px #D4F6FF, inset 0 0 40px #D4F6FF',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(-5px)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 0.5s ease-in-out',
       },
     },
+    fontFamily : {
+      com : ['Edu AU VIC WA NT PreM'],
+      description : ['Merienda'],
+      title : ['Bungee Shade']
+
+    },
+    letterSpacing : {
+      space : '0.35em',
+    },
+    lineHeight : {
+      '2.9' : '2.9em',
+    },
   },
-  plugins: [addVariablesForColors],
+  plugins: [
+    addVariablesForColors,
+    require('tailwindcss-textshadow'),
+    addVariablesForColors,
+  ],
 };
 
 function addVariablesForColors({ addBase, theme }: any) {
